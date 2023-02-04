0412e3436781370c7926cb56141e0d7d3a0a996b
erenkangal <123490653+erenkangal@users.noreply.github.com>
2023-02-04 19:12:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      linkedinLink: "https://www.linkedin.com/in/eren-kangal-b00aab1b4/",
+      instaLink: "https://www.instagram.com/ekangal1/",
+      twitterLink: "https://twitter.com/ekangal0",
+    };
+  },
+});
+
+app.mount("#user-goal");
+
